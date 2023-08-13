630ce0585b11e04413a2204df3d9c0df3e8f51f9
Matthew Salerno <m@salernosection.com>
2023-08-13 19:09:47 -0400

    add license

--- lib.nix
+++ lib.nix
@@ -1,3 +1,8 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 with builtins;
 /** 
 ACL independent functions that can be used in parsers.
