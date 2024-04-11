9112a7c3d0c28adf604c75d9a4cd2d19268aeff3
Conquerix <pierre@fournier.net>
2024-04-11 21:16:36 +0200

    Update static.nix better endpoint mkIf condition

--- configurers/static.nix
+++ configurers/static.nix
@@ -31,7 +31,7 @@ with getKeyProviderFuncs keyProviders inputs intermediateConfig localPeerName;
             publicKey = getPeerPubKey remotePeerName;
             presharedKeyFile = getSubnetPSKFile subnetName;
             allowedIPs = map ( ip: asCidr ip) peerConnection.ipAddresses;
-            endpoint = mkIf (peerConnection.endpoint != null) "${peerConnection.endpoint.ip}:${builtins.toString peerConnection.endpoint.port}";
+            endpoint = mkIf (peerConnection.endpoint != null && (peerConnection.endpoint ? ip) == true && (peerConnection.endpoint ? port) == true) "${peerConnection.endpoint.ip}:${builtins.toString peerConnection.endpoint.port}";
           }
           // (mergeIf peerConnection.endpoint "persistentKeepalive")
           // (mergeIf peerConnection.endpoint "dynamicEndpointRefreshSeconds")
