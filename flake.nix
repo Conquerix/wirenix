90af2dc061a4d749a98c9a51ede1afe509d45859
Pierre Fournier <pierre@fournier.net>
2024-04-11 19:32:56 +0200

    fixed flake's nixpkgs pointing to local path

--- flake.nix
+++ flake.nix
@@ -5,6 +5,9 @@
  */
 {
   description = "A wireguard network creation tool";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
   outputs = { self, nixpkgs, ... }:
   let
     forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
